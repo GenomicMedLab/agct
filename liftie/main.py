c3700746fc30b45f028e3e380af4bfc257a91f0d
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-09-20 00:28:00 -0700

    iterate on interface

---
+++ liftie/main.py
@@ -0,0 +1,32 @@
+"""Perform chainfile-driven liftover."""
+from enum import Enum
+
+import liftie._liftie as _liftie
+
+
+class Strand(str, Enum):
+
+    SENSE = "+"
+    ANTISENSE = "-"
+
+
+class Liftie:
+    """Liftover provider for a single DB to DB association."""
+
+    def __init__(self, from_db: str, to_db: str) -> None:
+        """Initialize liftover instance.
+
+        :param from_db: database name, e.g. ``"hg19"``
+        :param to_db: database name, e.g. ``"hg19"``
+        """
+        pass
+
+    def liftover(self, chrom: str, pos: int, strand: Strand = Strand.SENSE) -> str:
+        """Perform liftover for given params
+
+        :param chrom: chromosome name as given in chainfile. Usually e.g. ``"chr7"``.
+        :param pos: query position
+        :param strand: query strand (``"+"`` by default)
+        :return: first match TODO return whole list
+        """
+        return _liftie.liftover(chrom, pos)
--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,14 @@ use std::fs::File;
 use std::io::BufReader;
 
 #[pyfunction]
-fn do_liftover() -> PyResult<String> {
+fn liftover(chrom: &str, pos: usize) -> PyResult<String> {
     let data = BufReader::new(File::open("hg19ToHg38.over.chain").unwrap());
     let reader = chain::Reader::new(data);
     let machine = chain::liftover::machine::Builder::default().try_build_from(reader).unwrap();
 
-    let interval = "chr7:140453136".parse::<chain::core::Interval>().unwrap();
+    let interval_string: String = format!("{}:{}", chrom, pos);
+
+    let interval = interval_string.parse::<chain::core::Interval>().unwrap();
     for result in machine.liftover(&interval).unwrap() {
         return Ok(result.query().to_string())
     }
@@ -17,7 +19,7 @@ fn do_liftover() -> PyResult<String> {
 }
 
 #[pymodule]
-fn liftie(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(do_liftover, m)?)?;
+fn _liftie(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(liftover, m)?)?;
     Ok(())
 }
