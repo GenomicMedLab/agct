2586bede2352139a408b09c78b94d0b2ae23f402
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-09-20 09:12:17 -0700

    refactor: handle chainfile acquisition separately

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
 //! Provide Rust-based liftover classes.
-use pyo3::prelude::*;
 use chainfile as chain;
+use pyo3::prelude::*;
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
+
+/// Acquire chainfile.
+/// TODO: fetch from remote if not available locally, probably via config
+/// TODO: throw exceptions if unable to acquire
+fn get_chainfile(from_db: &str, to_db: &str) -> String {
+    let base_chainfile_dir = "~/.local/share/liftie/";
+    fs::create_dir_all(base_chainfile_dir).unwrap();
+    let path = format!(
+        "{}/hg{}ToHg{}.over.chain",
+        base_chainfile_dir, from_db, to_db
+    );
+    if Path::new(&path).exists() {
+        path
+    } else {
+        "this isn't going to work".to_string() // TODO fetch from remote
+    }
+}
 
 /// Define core Lifter class to be used by Python interface.
 #[pyclass]
@@ -14,9 +33,12 @@ pub struct Lifter {
 impl Lifter {
     #[new]
     pub fn new(from_db: &str, to_db: &str) -> Lifter {
-        let data = BufReader::new(File::open("hg38ToHg19.over.chain").unwrap());
+        let chainfile_name: String = get_chainfile(from_db, to_db);
+        let data = BufReader::new(File::open(&chainfile_name).unwrap());
         let reader = chain::Reader::new(data);
-        let machine = chain::liftover::machine::Builder::default().try_build_from(reader).unwrap();
+        let machine = chain::liftover::machine::Builder::default()
+            .try_build_from(reader)
+            .unwrap();
         Lifter { machine }
     }
 
@@ -25,7 +47,7 @@ impl Lifter {
 
         let interval = interval_string.parse::<chain::core::Interval>().unwrap();
         for result in self.machine.liftover(&interval).unwrap() {
-            return Ok(result.query().to_string())
+            return Ok(result.query().to_string());
         }
         Ok("".to_string())
     }
@@ -34,7 +56,6 @@ impl Lifter {
 /// Liftie Python module. Collect Python-facing methods.
 #[pymodule]
 fn _liftie(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(liftover, m)?)?;
     m.add_class::<Lifter>()?;
     Ok(())
 }
