3786ee41e5a9541d76996120edfd8e3204b3afd6
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-01-17 20:51:27 -0500

    enhancement: add edge case error handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,19 @@
 import os
 from pathlib import Path
 
+import pytest
+
+DATA_DIR = Path(__file__).parents[0] / "data"
 
 def pytest_sessionstart(session):
     """Perform actions after Session object is created.
 
     * set test data directory
     """
-    os.environ["WAGS_TAILS_DIR"] = str(Path(__file__).parents[0] / "data")
+    os.environ["WAGS_TAILS_DIR"] = str(DATA_DIR)
+
+
+@pytest.fixture(scope="module")
+def data_dir():
+    """Provide test data directory as a fixture."""
+    return DATA_DIR
---
+++ tests/test_rust_api.py
@@ -0,0 +1,12 @@
+"""Test some non-public aspects of the Rust layer."""
+import pytest
+
+from chainlifter._core import ChainfileError, ChainLifter
+
+
+def test_open_chainfile_errors(data_dir):
+    """Test chainfile opening/reading errors."""
+    with pytest.raises(FileNotFoundError):
+        _ = ChainLifter(str(data_dir / "non_existent_chainfile.chain"))
+    with pytest.raises(ChainfileError):
+        ChainLifter(str(data_dir / "invalid_chainfile.chain"))
