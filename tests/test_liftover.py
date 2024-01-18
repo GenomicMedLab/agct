cc61492cb7755e1010b4740fe22ae8a1c6307ddb
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-01-18 09:31:34 -0500

    unnecessary asserts

--- tests/test_liftover.py
+++ tests/test_liftover.py
@@ -7,22 +7,18 @@ def test_hg19_to_hg38():
     ch = ChainLifter(Genome.HG19, Genome.HG38)
 
     result = ch.convert_coordinate("chr7", 140439611)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140739811", "+"]
 
     result = ch.convert_coordinate("chr7", 140439746)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140739946", "+"]
 
     result = ch.convert_coordinate("chr7", 140439703)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140739903", "+"]
 
     result = ch.convert_coordinate("chr7", 140453136)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140753336", "+"]
 
@@ -36,16 +32,13 @@ def test_hg38_to_hg19():
     ch = ChainLifter(Genome.HG38, Genome.HG19)
 
     result = ch.convert_coordinate("chr7", 140739811)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140439611", "+"]
 
     result = ch.convert_coordinate("chr7", 140759820)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "140459620", "+"]
 
     result = ch.convert_coordinate("chr7", 60878240)
-    assert result
     assert len(result) == 1
     assert result[0] == ["chr7", "61646115", "+"]
